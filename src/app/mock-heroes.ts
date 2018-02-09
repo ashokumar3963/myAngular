eb370baddf35dca7ec1ba0f5aa29ee24d8254289
Ashok Kumar <ashok.kumar1@globallogic.com>
2018-02-09 15:49:02 +0530

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+{id:11, name: 'Mr. Ashok'},
+{id:12, name: 'Michel'},
+{id:13, name: 'Mohit'},
+{id:14, name: 'Sonu'},
+{id:15, name: 'Rohit'},
+{id:16, name: 'Mr. X'},
+{id:17, name: 'Mr. Ashok'},
+{id:18, name: 'Mr. Ashok'},
+{id:19, name: 'Mr. Ashok'},
+{id:20, name: 'Mr. Ashok'},
+];
